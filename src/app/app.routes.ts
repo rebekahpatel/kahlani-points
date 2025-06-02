290d6b0c94d79d4bd618fa561fab21ba742d2b32
rebekahpatel <rebapatel@gmail.com>
2025-06-01 17:59:04 -0700

    Home Page built with button.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
-export const routes: Routes = [];
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page',
+    },
+]
+
+export default routeConfig;
